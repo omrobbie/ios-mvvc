3516b97579e982e395c7e5f648eb344745fddf16
omrobbie <putu.roby@yahoo.com>
2020-08-07 10:15:43 +0800

    call delegate to proceed the next action

--- ios-mvvc/Controller/ViewController.swift
+++ ios-mvvc/Controller/ViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var homeView: View!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeView.delegate = self
+    }
+}
+
+extension ViewController: ViewDelegate {
+
+    func didSelectRowAt(indexPath: IndexPath) {
+        let item = homeView.data[indexPath.row]
+        print("You are selected: \(item.name) - \(item.title)")
     }
 }
