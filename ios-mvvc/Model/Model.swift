0573ba9a903ace06865198d738d1275a4b074b7f
omrobbie <putu.roby@yahoo.com>
2020-08-07 09:35:05 +0800

    create data model

---
+++ ios-mvvc/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  ios-mvvc
+//
+//  Created by omrobbie on 07/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+struct Model {
+
+    let name: String
+    let title: String
+}
