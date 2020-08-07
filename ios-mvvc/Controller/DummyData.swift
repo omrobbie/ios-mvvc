5dc782d8abcadef0b7252ba0554e31558134607c
omrobbie <putu.roby@yahoo.com>
2020-08-07 09:44:48 +0800

    create dummy data

---
+++ ios-mvvc/Controller/DummyData.swift
@@ -0,0 +1,13 @@
+//
+//  DummyData.swift
+//  ios-mvvc
+//
+//  Created by omrobbie on 07/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+var dummyData: [Model] = [
+    Model(name: "omrobbie", title: "iOS Developer"),
+    Model(name: "Putu", title: "Balinese Name"),
+    Model(name: "Roby", title: "Secondary Name")
+]
